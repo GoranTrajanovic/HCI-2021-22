68e1676c4612948e465c457dcc382fe9ba4c6b6d
Goran Trajanovic <goran.trajanovic09@gmail.com>
2021-10-13 09:30:56 +0200

    Test git pre-commit hook 2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -11,25 +11,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    <main className={styles.main}>
-<h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Korisnicka sucelja!</a>
-    </h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Korisnicka sucelja!</a>
+        </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>
+              Find in-depth information about Next.js features and API.
+            </p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>
+              Learn about Next.js in an interactive course with quizzes!
+            </p>
           </a>
 
           <a
@@ -37,7 +41,9 @@ export default function Home() {
             className={styles.card}
           >
             <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>
+              Discover and deploy boilerplate example Next.js projects.
+            </p>
           </a>
 
           <a
@@ -46,7 +52,8 @@ export default function Home() {
           >
             <h2>Deploy &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Instantly deploy your Next.js site to a public URL with
+              Vercel.
             </p>
           </a>
         </div>
@@ -58,12 +65,17 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              width={72}
+              height={16}
+            />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
